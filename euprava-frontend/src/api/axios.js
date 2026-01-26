009e8ec9e72b3d0673fb1459c20a4c1190f8e484
Janko Mijailovic <4mukili4@gmail.com>
2026-01-26 14:49:22 +0100

    Konekcija sa backendom uspostavljena preko Axios biblioteke

---
+++ euprava-frontend/src/api/axios.js
@@ -0,0 +1,20 @@
+import axios from "axios";
+
+const api = axios.create({
+  baseURL: "http://127.0.0.1:8000",
+  headers: {
+    Accept: "application/json",
+    "Content-Type": "application/json",
+  },
+});
+
+// Ubaci token iz sessionStorage ako postoji.
+api.interceptors.request.use((config) => {
+  const token = sessionStorage.getItem("token");
+  if (token) {
+    config.headers.Authorization = `Bearer ${token}`;
+  }
+  return config;
+});
+
+export default api;
